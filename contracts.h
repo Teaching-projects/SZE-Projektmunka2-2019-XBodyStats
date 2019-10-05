933c82048109dde6fb994a5bcac639f1d8b4c361
Kearmin <kertesz.armin96@gmail.coom>
2019-10-05 15:28:27 +0200

    replaced Color enums to contracts + added startButton

--- contracts.h
+++ contracts.h
@@ -1,12 +1,21 @@
 #ifndef CONTRACTS_H
 #define CONTRACTS_H
 #include <qstring.h>
+#include <iostream>
 //TODO implement these
+
+enum Burst{Red,Green,Yellow};
+
+enum StartButtonState{Start, Pause};
 
 struct MuscleButtonProperties{
     bool isPressed;
     int buttonId;
     QString percent;
+
+    void printContent(){
+        std::cout << "button "<<buttonId << " ispressed: "<< isPressed << " percent "<< percent.toUtf8().constData() << std::endl;
+    }
 };
 
 struct MasterButton{
@@ -28,6 +37,7 @@ struct ViewContent  {
    QString clock;
    BurstTimeIndicator timeIndicator;
    MasterButton masterButton;
+   StartButtonState startButtonState;
    //int slideValue; only if needed
 };
 
