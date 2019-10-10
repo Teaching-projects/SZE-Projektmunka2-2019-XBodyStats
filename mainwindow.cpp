112d7e8de03f8340e3a6a165db89498dfa7098db
Kozz <ko.z.i@hotmail.com>
2019-10-10 16:43:16 +0200

    Added Workout Interface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <qstring.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,15 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    delegate->masterButtonPressed();
+
+}
+
+void MainWindow::display(ViewContent *v)
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,18 +2,27 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "exerciseviewdelegate.h"
+#include "baseexerciseview.h"
+#include "contracts.h"
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow, public BaseExerciseView
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    ExerciseViewDelegate* delegate;
+
+    void display(ViewContent* v);
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
