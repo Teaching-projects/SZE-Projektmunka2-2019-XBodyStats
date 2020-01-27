573041badaa78120819a0c2ca96d0a53496ec731
Kozz <ko.z.i@hotmail.com>
2020-01-27 17:45:28 +0100

    no message

--- contracts.h
+++ contracts.h
@@ -3,6 +3,7 @@
 #include <qstring.h>
 #include <qcolor.h>
 #include <iostream>
+#include <vector>
 //TODO implement these
 
 enum Burst{Red,Green,Yellow};
@@ -67,7 +68,7 @@ struct MasterButtonModel{
 
 struct ExerciseSecond{
     int time;
-    MuscleModel* muscles;
+    std::vector<MuscleModel*> muscles;
     MasterButtonModel master;
 };
 
@@ -82,6 +83,6 @@ struct ModelData{
     std::string fileName;
     User user;
     ExerciseType type;
-    ExerciseSecond* seconds;
+    std::vector<ExerciseSecond*> seconds;
 };
 #endif // CONTRACTS_H
