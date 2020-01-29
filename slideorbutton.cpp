36f408a090f94977c084beb9ddb065c61a0f32c0
Kozz <ko.z.i@hotmail.com>
2020-01-29 17:48:14 +0100

    slider

--- slideorbutton.cpp
+++ slideorbutton.cpp
@@ -1,4 +1,5 @@
 #include "slideorbutton.h"
+#include <sstream>
 
 SlideOrButton::SlideOrButton(std::vector<ModelData*>& _data, AlgorithmParams* params):Algorithm (_data,params){}
 
@@ -20,8 +21,16 @@ std::string SlideOrButton::run(){
         }
     }
     double sum = buttonpercent+sliderpercent;
-    std::string s =  "A valtoztatasok " + std::to_string(double((buttonpercent/sum)*100.0)) + "%-a tortent gombbal, mig " + std::to_string(double((sliderpercent/sum)*100.0)) + "%-a csuszkaval.";
-    return "\n\n\n-----------------\nOutput of SlideOrButton:\nA valtoztatasok " + std::to_string(int((buttonpercent/sum)*100)) + "%-a tortent gombbal, mig " + std::to_string(int((sliderpercent/sum)*100)) + "%-a csuszkaval.";
+    std::string s;
+    double button = (double)((buttonpercent/sum)*100.0);
+    double slider = (double)((sliderpercent/sum)*100.0);
+    std::stringstream ss("");
+    ss << "\n\n\n-----------------\nOutput of SlideOrButton:\nA valtoztatasok " << std::setprecision(4) << button << "%-a tortent gombbal, mig " << std::setprecision(4) << slider << "%-a csuszkaval.";
+
+    s =  "A valtoztatasok " + std::to_string(double((buttonpercent/sum)*100.0)) + "%-a tortent gombbal, mig " + std::to_string(double((sliderpercent/sum)*100.0)) + "%-a csuszkaval.";
+    //return "\n\n\n-----------------\nOutput of SlideOrButton:\nA valtoztatasok " + std::to_string(int((buttonpercent/sum)*100)) + "%-a tortent gombbal, mig " + std::to_string(int((sliderpercent/sum)*100)) + "%-a csuszkaval.";
+    s = ss.str();
+    return s;
 }
 
 SlideOrButton::~SlideOrButton(){
